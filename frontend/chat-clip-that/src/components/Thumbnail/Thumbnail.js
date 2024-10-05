e5d3ba3690f71b18be7bf0af3ed1e91563b955d9
CJ Cosby <charlescjcosby@gmail.com>
2024-10-05 17:02:13 -0400

    Some fixes

--- frontend/chat-clip-that/src/components/Thumbnail/Thumbnail.js
+++ frontend/chat-clip-that/src/components/Thumbnail/Thumbnail.js
@@ -2,18 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Display } from 'phaser';
 
-const style = {
-   width: '20vw',
-   height: '85%',
-   backgroundColor: 'blue',
-   // margin: '1% 2% ',
-   // display: 'inline-block',
-   // whiteSpace: 'normal',
-   // overflow: 'hidden',
-};
 
 const Thumbnail = () => (
-   <iframe src="https://i.pinimg.com/originals/14/84/ae/1484ae77d2f40286b01089c31339b7b2.jpg" class="w-1/5 bg-blue-500 h-4/6 mx-9 inline-block whitespace-normal align-middle flex-shrink-0">
+   <iframe src="https://i.pinimg.com/originals/14/84/ae/1484ae77d2f40286b01089c31339b7b2.jpg" class="w-1/5 bg-blue-500 h-5/6 mx-9 inline-block whitespace-normal align-middle flex-shrink-0">
       VideoBar Component
    </iframe>
 );
--- frontend/chat-clip-that/src/components/videoBar/VideoBar.js
+++ frontend/chat-clip-that/src/components/videoBar/VideoBar.js
@@ -5,20 +5,23 @@ import '../component.css';
 import PropTypes from 'prop-types';
 
 const VideoBar = (props) => (
-   <section className="w-ful h-[30vh] place-content-around place-items-center mb-96 flex">
-      <button onClick={() => sideScrollVidBar(props.id, true)} class="h-1/4 ml-12 w-[3.5%] rounded-full align-middle">&lt;</button>
-      <div id={props.id} class="overflow-x-scroll w-10/12 h-full overflow-y-hidden whitespace-nowrap mx-auto flex place-items-center">
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
-         <Thumbnail />
+   <section>
+      <h2 class="text-left text-4xl pl-44">Aasdfasdf</h2>
+      <div className="w-ful h-[27vh] place-content-around place-items-center mb-96 flex">
+         <button onClick={() => sideScrollVidBar(props.id, true)} class="h-1/4 ml-12 w-[3.5%] rounded-full align-middle">&lt;</button>
+         <div id={props.id} class="overflow-x-scroll w-10/12 h-full overflow-y-hidden whitespace-nowrap mx-auto flex place-items-center">
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+            <Thumbnail />
+         </div>
+         <button onClick={() => sideScrollVidBar(props.id, false)} class="h-1/4 mr-12 w-[3.5%] rounded-full align-middle">&gt;</button>
       </div>
-      <button onClick={() => sideScrollVidBar(props.id, false)} class="h-1/4 mr-12 w-[3.5%] rounded-full align-middle">&gt;</button>
 
    </section>
 
